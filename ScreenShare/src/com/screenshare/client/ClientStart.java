b26f8da3e61f7cff65003a94433daabd25c49425
viggnesh.venu@gmail.com <viggnesh.venu@gmail.com@638d3909-e014-d34f-dec7-abd39ade0f27>
2013-08-05 08:58:52 +0000

    made basic final changes

--- ScreenShare/src/com/screenshare/client/ClientStart.java
+++ ScreenShare/src/com/screenshare/client/ClientStart.java
@@ -11,8 +11,8 @@ public class ClientStart {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Client testServer;
+		// code to start the client.java file
+		/*Client testServer;
 		try {
 			testServer = new Client();
 			testServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,11 +20,29 @@ public class ClientStart {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		
+		//code to start the ClientRecieve.java file
+		ClientRecieve testClientRecieve;
+		try {
+			testClientRecieve = new ClientRecieve();
+			testClientRecieve.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			testClientRecieve.clientRunner();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		//starts the server
-		//testServer.Runner();
-
+		//code to start ClientSend.java file
+		/*ClientSend testClientSend;
+		try {
+			testClientSend = new ClientSend();
+			testClientSend.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			testClientSend.clientRunner();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 	}
 
 }
